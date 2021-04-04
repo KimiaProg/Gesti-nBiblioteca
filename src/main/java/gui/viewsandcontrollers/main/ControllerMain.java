25a3db7173297f3ac40317ae1056ef4094ee3374
Kimia <kimia.ehsani.dolati.al@iespoligonosur.org>
2021-04-04 13:16:50 +0200

    the first

---
+++ src/main/java/gui/viewsandcontrollers/main/ControllerMain.java
@@ -0,0 +1,39 @@
+package gui.viewsandcontrollers.main;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+public class ControllerMain {
+	
+	@FXML
+	private TextField text;
+	
+	@FXML
+	private Window root;
+    public void initialize() {
+        // TODO
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+    	Node source = (Node) event.getSource();
+	    Stage parent = (Stage) source.getScene().getWindow();
+    	
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../modal/SecondFXML.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage dialog = new Stage();
+        dialog.setScene(new Scene(root1)); 
+        dialog.setTitle(text.getText());
+        dialog.initOwner(parent);
+        dialog.initModality(Modality.APPLICATION_MODAL); 
+        dialog.showAndWait();
+    }
+   
+}
---
+++ src/main/java/gui/viewsandcontrollers/modal/ControllerSecond.java
@@ -0,0 +1,22 @@
+package gui.viewsandcontrollers.modal;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class ControllerSecond {
+
+	public void initialize() {
+		// TODO
+	}
+
+	@FXML
+	private void cerrarVentana(ActionEvent event) throws IOException {
+		Node source = (Node) event.getSource();
+	    Stage stage = (Stage) source.getScene().getWindow();
+	    stage.close();
+	}
+
+}
